7d9c58532ad38876177720fa85d462700818bbba
rofluffy <shirley5001@hotmail.com>
2014-10-28 15:20:15 -0700

    fix conflict try 3

---
+++ src/com/project/LibraryLocator/client/LoginServiceAsync.java
@@ -0,0 +1,7 @@
+package com.project.LibraryLocator.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface LoginServiceAsync {
+  public void login(String requestUri, AsyncCallback<LoginInfo> async);
+}
